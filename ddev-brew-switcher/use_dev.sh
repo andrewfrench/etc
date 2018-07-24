2572a107a5df4ac2be85187b1533199a060fb7db
Andrew French <andrew@afren.ch>
2018-07-24 16:04:00 -0600

    Update dev binary path.

--- ddev-brew-switcher/use_dev.sh
+++ ddev-brew-switcher/use_dev.sh
@@ -4,5 +4,5 @@ set -x
 set -o errexit
 
 rm -f /usr/local/bin/ddev
-ln -s /usr/local/bin/ddev.link /usr/local/bin/ddev
+ln -s ~/go/src/github.com/drud/ddev/bin/darwin/darwin_amd64/ddev /usr/local/bin/ddev
 
